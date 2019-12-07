e1e9c1e7100f9d42c0479102023f56b884884b90
cecilio90 <o.cecilio90@gmail.com>
2019-12-07 17:32:39 -0300

    Created Footer component (10m)

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Header from './Header'
+import Footer from './Footer'
 import '../App.scss';
 
 const App = () =>{
   return (
     <div className="main">
       <Header />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <small>Copyright © 2019 LenioTest.</small> 
+        </div>
+    )
+};
+
+export default Footer;

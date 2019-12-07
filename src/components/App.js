dafec23ee5e6351e62213026213332e2c2558931
cecilio90 <o.cecilio90@gmail.com>
2019-12-07 17:18:10 -0300

    Created header (20m)
    Install sass dependency

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Header from './Header'
+import '../App.scss';
+
+const App = () =>{
+  return (
+    <div className="main">
+      <Header />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header__item">
+                Members
+            </div>
+            <div className="header__menu">
+                <div className="header__item">
+                    All Members
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Header;
